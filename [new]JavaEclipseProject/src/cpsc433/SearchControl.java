bf83ae25000259f8f49da8638f3fb4f9e66d951f
Chris Hawk <chrishawk9@gmail.com>
2016-11-22 07:04:34 -0700

    Added search control stub

---
+++ [new]JavaEclipseProject/src/cpsc433/SearchControl.java
@@ -0,0 +1,12 @@
+package cpsc433;
+
+public class SearchControl {
+	//f_select
+		//return child with best f_wert score
+	//f_wert
+		//return sum of penalties for violated penalties 
+	//constraint1
+		//return penalty if violated
+	//constraint2
+	//...
+}
