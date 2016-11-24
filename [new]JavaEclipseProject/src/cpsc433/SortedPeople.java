94658b78f0af1cbcdf0c3ddfef5fc6b65c902324
christopher.hawk <christopher.hawk@ucalgary.ca>
2016-11-23 17:37:17 -0700

    Finished SortedPeople

--- [new]JavaEclipseProject/src/cpsc433/SortedPeople.java
+++ [new]JavaEclipseProject/src/cpsc433/SortedPeople.java
@@ -54,17 +54,52 @@ public class SortedPeople {
 					noAttributes.put(p.getName(), p);
 			}					
 		}
-		
+	}
 	/**
 	 * Removes and returns the next highest-ranking person. 
 	 */
 	public Person next(){
-		
-	}
 		if(!groupHeads.isEmpty()){
-			Person p = groupHeads. 
-		}
-			
+			for(Person p: groupHeads.values())
+				return p;
+			}
+		else if(!projHeads.isEmpty()){
+			for(Person p: projHeads.values())
+				return p;
+			}
+		else if(!managers.isEmpty()){
+			for(Person p: managers.values())
+				return p;
+			}
+		else if(!secretaries.isEmpty()){
+			for(Person p: secretaries.values())
+				return p;
+			}
+		else if(!smokers.isEmpty()){
+			for(Person p: smokers.values())
+				return p;
+			}
+		else if(!hackers.isEmpty()){
+			for(Person p: hackers.values())
+				return p;
+			}
+		else if(!researchers.isEmpty()){
+			for(Person p: researchers.values())
+				return p;
+			}
+		else if(!noAttributes.isEmpty()){
+			for(Person p: noAttributes.values())
+				return p;
+			}
+	
+		return null;
 	}
-
+	
+	public boolean isEmpty(){
+		if (next()==null)
+			return true;
+		else
+			return false;
+	}
 }
+
