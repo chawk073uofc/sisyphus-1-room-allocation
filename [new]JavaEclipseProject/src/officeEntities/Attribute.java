65f244204cc0cf20fc52bc18bf12e5773c2ec21a
Chris Hawk <chrishawk9@gmail.com>
2016-11-28 13:41:48 -0700

    Attribute enum added. Still sloving some errors.

---
+++ [new]JavaEclipseProject/src/officeEntities/Attribute.java
@@ -0,0 +1,16 @@
+package officeEntities;
+
+public enum Attribute {
+	PERSON("person"),
+	RESEARCHER("researcher"),
+	HACKER("hacker"),
+	SMOKER("smoker"),
+	SECRETARY("secretary"),
+	MANAGER("manager"),
+	PROJECT_HEAD("project-head"),
+	GROUP_HEAD("group-head");
+	
+	private String displayName;
+	Attribute(String displayName){this.displayName = displayName;}
+	@Override public String toString() {return displayName;}
+}
