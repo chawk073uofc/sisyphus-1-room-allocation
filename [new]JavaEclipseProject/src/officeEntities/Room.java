fb321b5a4b9b44fa26ea64b07a05de60b425e8d7
christopher.hawk <christopher.hawk@ucalgary.ca>
2016-12-05 17:29:31 -0700

    Room commented. Person fields commented. Unused imports removed. One unused method deleted.

--- [new]JavaEclipseProject/src/officeEntities/Room.java
+++ [new]JavaEclipseProject/src/officeEntities/Room.java
@@ -2,27 +2,29 @@
 package officeEntities;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeSet;
-
 import cpsc433.Entity;
-import officeEntities.Room.RoomSize;
 
 /**
- * This class represents a room in the office allocation problem. It organizes important data
+ * <p>This class represents a room in the office allocation problem. It organizes important data
  * such as a list of the people currently assigned to this room and another list of rooms that are 
- * close to a particular room. The size of the room is also stored here. The class includes
+ * close to a particular room. The size of the room is also stored here. The default room size is medium. The class includes
  * a static array list with all Room instances created so far.
- * @author Brandon Sieu
- *
+ * 
+ * <p>The use of {@link java.util.HashMap} in this class allows for constant time checking if a room with a given name has already 
+ * been created which is critical during the input file parsing phase of the program when this must be done every time a room-related
+ * predicate is encountered. 
  */
 
 public class Room extends Entity{
-	private static Map<String,Room> rooms =new HashMap<>(); //All instances of class Room currently instantiated.
+	/**All instances of class Room currently instantiated*/
+	private static Map<String,Room> rooms =new HashMap<>();
+	/**All people assigned to this room.*/
 	private Map<String, Person> occupants = new HashMap<>();
+	/**All rooms close to this room*/
 	private Map<String, Room> closeTo = new HashMap<>();
+	/**The {@link RoomSize size} of this room*/
 	private RoomSize size;
 	
 	/**
@@ -53,7 +55,14 @@ public class Room extends Entity{
 	public static boolean exists(String name){
 		return rooms.containsKey(name);
 	}
-	
+	/**
+	 * Compares this Room object to the given object on the basis of the sizes of these rooms (see {@link RoomSize}).
+	 * @param r	room object to compare
+	 * @return negative number if r is larger than this room
+	 * 		   positive number if r is smaller than this room
+	 *   	   zero	rooms are of equal size
+	 * @throws ClassCastException if the given Entity object is not an instance of Room
+	 */
 	@Override
 	public int compareTo(Entity r){
 	    if (r instanceof Room) {
@@ -67,29 +76,22 @@ public class Room extends Entity{
 			}
 		    else throw new java.lang.ClassCastException();
 		}
-	
-//	@Override
-//	public boolean equals(Room arg0){
-//		return this.equals(arg0);
-//	}
 	/**
 	 * Returns true if a room object is close to the calling room object.
 	 * @param checkRoom room to check close to relation with.
 	 * @return true if a room object is close to the calling room object.
 	 */
 	public boolean isCloseTo(Room checkRoom){
-		return closeTo.containsKey(checkRoom.getName());//O(1)
+		return closeTo.containsKey(checkRoom.getName());
 	}
-	
 	/**
 	 * Adds a Room object to the calling Room object's close to array.
 	 * @param neighbour room to add to closeTo array.
 	 */
 	public void addCloseTo(Room neighbour){
-		if(!closeTo.containsKey(neighbour.getName())) //O(1)
-			closeTo.put(neighbour.getName(), neighbour);//O(1)
+		if(!closeTo.containsKey(neighbour.getName())) 
+			closeTo.put(neighbour.getName(), neighbour);
 	}
-	
 	/**
 	 * Gets the Room object given a name.
 	 * @param roomName name of the room to get the Room object of.
@@ -102,7 +104,6 @@ public class Room extends Entity{
 			throw new NoSuchRoomException();
 		return rm;
 	}
-
 	/**
 	 * Returns the room size of the calling Room object.
 	 * @return size of the room.
@@ -110,7 +111,6 @@ public class Room extends Entity{
 	public RoomSize getSize() {
 		return size;
 	}
-
 	/**
 	 * Sets the room size of the calling Room object.
 	 * @param size of the room to set.
@@ -118,7 +118,6 @@ public class Room extends Entity{
 	public void setSize(RoomSize size) {
 		this.size = size;
 	}
-	
 	/**
 	 * Adds a person to the calling Room object's occupants array.
 	 * @param p the person to add.
@@ -126,7 +125,6 @@ public class Room extends Entity{
 	public void addPerson(Person p){
 		occupants.put(p.getName(), p);
 	}
-	
 	/**
 	 * Checks if a person exists in the calling Room object's occupants array.
 	 * @param p the person to check.
@@ -138,7 +136,6 @@ public class Room extends Entity{
 		}
 		return false;
 	}
-	
 	/**
 	 * Returns a string with all the information relating to this room.
 	 * @return roomStr the information string pertaining to a room.
@@ -155,7 +152,6 @@ public class Room extends Entity{
 		roomStr += "\n";
 		return roomStr;
 	}
-
 	/**
 	 * Returns a string with all the information relating to all the rooms.
 	 * @return the string with all the information relating to all the rooms.
@@ -168,7 +164,6 @@ public class Room extends Entity{
 		
 		return roomsStr;
 	}
-	
 	/**
 	 * Represents all valid room sizes. Provides appropriate string representations of each. 
 	 *
@@ -179,29 +174,33 @@ public class Room extends Entity{
 		RoomSize(String displayName){this.displayName = displayName;}
 		@Override public String toString() {return displayName;}
 	}
-	
 	/**
 	 * Adds a person to the calling Room object's occupant array.
 	 * @param personMovingIn the person to be put into the room.
 	 */
 	public void addOccupant(Person personMovingIn) {
-		if(!occupants.containsKey(personMovingIn.getName()))//O(1)
-			occupants.put(personMovingIn.getName(), personMovingIn);	//O(1)
+		if(!occupants.containsKey(personMovingIn.getName()))
+			occupants.put(personMovingIn.getName(), personMovingIn);
 	}
-	
+	/**
+	 * Assesses the maximum number people that can be accommodated by the rooms in the knowledge base, assuming there are no bosses.
+	 * @return maxPeople the maximum number people that can be accommodated by the rooms in the knowledge base, assuming there are no bosses.
+	 */
 	public static int buildingCapacity() {
 		return rooms.size() * 2;
 	}
-	
+	/**
+	 * Getter for the number of rooms currently in the knowledge base. 
+	 * @return numRooms the number of rooms currently in the knowledge base
+	 */
 	public static int numberOfRooms() {
 		return rooms.size();
 	}
 	/**
 	 * Returns true if no more people may be added to the room and false otherwise. 
-	 * @return true if no more people may be added to the room.
+	 * @return true if two people have already been assigned to this room or if the sole occupant is a {@link Person#isBoss() boss}.
 	 */
 	public boolean isFull(){
-		//System.out.println("Room:" + this.getName() + " occSize:" + occupants.size());
 		if (occupants.size() == 2 || occupantIsBoss())
 			return true;
 		else
@@ -222,15 +221,17 @@ public class Room extends Entity{
 			return false;
 		}
 	}
-	
-	public static HashMap<String, Room> getRooms(){
-		return (HashMap<String, Room>) rooms;
-	}
-	
+	/**
+	 * Getter for the occupants of this room.
+	 * @return occupants the list of people assigned to this room.
+	 */
 	public Map<String, Person> getOccupants(){
 		return occupants;
 	}
-	
+	/**
+	 * Compiles a list of rooms that are not {@link #isFull() full}.
+	 * @return availableRooms a list of rooms that are not {@link #isFull() full}.
+	 */
 	public static ArrayList<Room> getAvailableRooms(){
 		ArrayList<Room> availableRooms = new ArrayList<>();
 		for(Room r: rooms.values()){
@@ -240,17 +241,22 @@ public class Room extends Entity{
 		}
 		return availableRooms;
 	}
-	
+	/**
+	 * Checks if a given list of rooms contains at least room that is not {@link #isFull() full}.
+	 * @param roomsToCheck a list of rooms
+	 * @return true if the given list of rooms contains at least room that is not {@link #isFull() full}.
+	 */
 	public static boolean hasEmptyRoom(ArrayList<Room> roomsToCheck){
 		for(Room r: roomsToCheck){
 			if(r.isEmptyRoom()) return true;
 		}
 		return false;
 	}
-	
+	/**
+	 * Checks if this room is empty. 
+	 * @return true if this room has no occupants
+	 */
 	public boolean isEmptyRoom(){
 		return (occupants.size()==0);
 	}
-	
-	
 }
